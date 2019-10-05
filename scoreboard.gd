5c74075845adeaebbe69c0aa1aa0a914c805c365
A.Bond <alexey.bond.94.55@gmail.com>
2019-10-06 00:06:11 +0600

    Add leaderboard

--- scoreboard.gd
+++ scoreboard.gd
@@ -2,16 +2,55 @@ extends Node
 
 onready var textNode = get_node('text')
 
-signal current_score
-
 var current_score = 0
+
+var all_scores = []
+var top_scores
+
+func read_scores():
+	var file = File.new()
+	file.open('user://catscores.json', File.READ)
+	var pr = JSON.parse(file.get_as_text())
+	if pr.result:
+		all_scores = pr.result
+	else:
+		all_scores = []
+	file.close()
+
+func write_scores():
+	var file = File.new()
+	file.open('user://catscores.json', File.WRITE)
+	file.store_string(JSON.print(all_scores))
+	file.close()
+
+func refresh_top():
+	var s = all_scores.duplicate()
+	s.sort()
+	s.invert()
+	var ss = []
+	# fck ths sht
+	for i in range(5):
+		if s.size() > i:
+			ss.append(s[i])
+	top_scores = ss
+
+	var msg = ''
+	for r in ss:
+		msg += ' %d\n' % r
+	textNode.text = msg
 
 func update_current(score):
 	current_score += score
-	textNode.text = '%d' % current_score
+	textNode.text = ' %d' % current_score
 
 func gameover():
-	pass
+	all_scores.append(current_score)
+	current_score = 0
+	write_scores()
 
 func get_top():
 	return [1, 2, 3, 4, 5]
+
+func _ready():
+	read_scores()
+	refresh_top()
